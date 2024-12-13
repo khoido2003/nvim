a3a74fac97a7d7a195f832b87655fdaa644f9f0f
Khoi Do <khoizpro31@gmail.com>
2024-12-13 17:40:18 +0700

    Update mocha flavour color scheme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -194,14 +194,14 @@ require("catppuccin").setup({
 		dark = "mocha",
 	},
 	transparent_background = false, -- disables setting the background color.
-	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+	term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
 	dim_inactive = {
-		enabled = false, -- dims the background color of inactive window
+		enabled = true, -- dims the background color of inactive window
 		shade = "dark",
 		percentage = 0.15, -- percentage of the shade to apply to the inactive window
 	},
-	no_italic = false, -- Force no italic
+	no_italic = true, -- Force no italic
 	no_bold = false, -- Force no bold
 	no_underline = true, -- Force no underline
 	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
@@ -221,9 +221,9 @@ require("catppuccin").setup({
 	},
 	color_overrides = {
 		mocha = {
-			base = "#18191a",
-			mantle = "#18191a",
-			crust = "#18191a",
+			base = "#181820",
+			mantle = "#181820",
+			crust = "#181820",
 		},
 	},
 	custom_highlights = {},
@@ -244,6 +244,10 @@ require("catppuccin").setup({
 
 -- setup must be called before loading
 vim.cmd.colorscheme("catppuccin")
+
+-------------------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------------------
 
 -- CONFIGURATIONS
 
