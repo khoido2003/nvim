e52825b48a3531cd4dd0e7166da50bf789ed348c
khoido2003 <khoizpro31@gmail.com>
2025-01-24 21:42:14 +0700

    Update lualine show relative path

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -316,7 +316,7 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
