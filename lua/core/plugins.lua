dbd2c1c398673d5dc298a1da0559ea16da00222d
khoido2003 <khoizpro31@gmail.com>
2025-01-26 21:43:47 +0700

    Change gitsign plugins display signs

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -370,16 +370,16 @@ require("nvim-tree").setup({
 -- GIT Support
 require("gitsigns").setup({
 	signs = {
-		add = { text = "┃" },
-		change = { text = "┃" },
+		add = { text = "+" },
+		change = { text = "~" },
 		delete = { text = "_" },
 		topdelete = { text = "‾" },
 		changedelete = { text = "~" },
 		untracked = { text = "┆" },
 	},
 	signs_staged = {
-		add = { text = "┃" },
-		change = { text = "┃" },
+		add = { text = "+" },
+		change = { text = "~" },
 		delete = { text = "_" },
 		topdelete = { text = "‾" },
 		changedelete = { text = "~" },
