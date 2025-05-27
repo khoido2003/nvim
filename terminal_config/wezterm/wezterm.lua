6b9f20b9114be7d2692cedff35a327e1016d87e6
khoido2003 <khoizpro31@gmail.com>
2025-05-27 23:47:47 +0700

    Update wezterm

--- terminal_config/wezterm/wezterm.lua
+++ terminal_config/wezterm/wezterm.lua
@@ -2,13 +2,15 @@ local wezterm = require("wezterm")
 -- local gpus = wezterm.gui.enumerate_gpus()
 
 local config = {
+	enable_wayland = false, -- Wayland is not used on Windows
+	check_for_updates = false,
+
 	window_padding = {
 		left = 0,
 		right = 0,
 		top = 0,
 		bottom = 0,
 	},
-
 	show_close_tab_button_in_tabs = false,
 	front_end = "OpenGL",
 	max_fps = 60,
@@ -22,8 +24,8 @@ local config = {
 
 	-- Font settings
 	-- font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" }),
-	font = wezterm.font("Consolas", { weight = "Regular" }),
-	font_size = 11.5,
+	-- font = wezterm.font("Consolas", { weight = "Regular" }),
+	font_size = 10.5,
 
 	window_close_confirmation = "NeverPrompt",
 	scrollback_lines = 100,
