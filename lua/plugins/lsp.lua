2b231eb51c4662f4b350b68aca24f7706cb0ad43
khoido2003 <khoizpro31@gmail.com>
2025-07-09 21:12:36 +0700

    improve(lsp): disable unused features

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -76,6 +76,10 @@ return {
 		local on_attach = function(client, _)
 			client.server_capabilities.documentFormattingProvider = false
 			client.server_capabilities.documentRangeFormattingProvider = false
+			client.server_capabilities.codeLensProvider = false
+			client.server_capabilities.documentHighlightProvider = false
+			client.server_capabilities.foldingRangeProvider = false
+			client.server_capabilities.inlayHintProvider = false
 		end
 
 		local servers = {
