bb30d80fe736aaf8fd66d103d320fe3649349451
khoido2003 <khoizpro31@gmail.com>
2025-08-06 23:25:38 +0700

    refactor(lualine): change lualine layout, remove inclinenvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,7 @@ return {
 	require("plugins.diffview"),
 	require("plugins.nvim_tree"),
 	require("plugins.treesitter"),
-	require("plugins.statusline"),
+	require("plugins.lualine"),
 	require("plugins.telescope"),
 	require("plugins.mason"),
 	require("plugins.formatter"),
@@ -13,5 +13,4 @@ return {
 	require("plugins.lsp"),
 	require("plugins.git_signs"),
 	require("plugins.trouble"),
-	require("plugins.incline"),
 }
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,147 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = { "NvimTree", "TelescopePrompt" },
+				always_divide_middle = true,
+				always_show_tabline = false,
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = {
+					{
+						function()
+							local mode = vim.api.nvim_get_mode().mode
+							local mode_map = {
+								n = "N",
+								no = "N",
+								nov = "N",
+								niI = "N",
+								niR = "N",
+								niV = "N",
+								nt = "N",
+								v = "V",
+								vs = "V",
+								V = "V",
+								Vs = "V",
+								[""] = "V",
+								s = "S",
+								S = "S",
+								[""] = "S",
+								i = "I",
+								ic = "I",
+								ix = "I",
+								R = "R",
+								Rc = "R",
+								Rx = "R",
+								Rv = "R",
+								c = "C",
+								cv = "C",
+								ce = "C",
+								r = "R",
+								rm = "M",
+								["r?"] = "?",
+								["!"] = "!",
+								t = "T",
+							}
+							return mode_map[mode] or mode:sub(1, 1):upper()
+						end,
+						color = { gui = "bold" },
+					},
+				},
+
+				lualine_b = {
+					{
+						function()
+							local filename = vim.fn.expand("%:t")
+
+							local file_status = ""
+							if vim.bo.modified then
+								file_status = file_status .. " ●"
+							end
+							if vim.bo.readonly then
+								file_status = file_status .. " "
+							end
+							if filename == "" then
+								return "[No Name]"
+							end
+
+							local extension = vim.fn.expand("%:e")
+							local icon = require("nvim-web-devicons").get_icon(filename, extension, { default = true })
+								or ""
+
+							return icon .. " " .. filename .. file_status
+						end,
+						icon = "",
+					},
+				},
+
+				lualine_c = {},
+
+				lualine_x = {
+					{
+						function()
+							local bufnr = vim.api.nvim_get_current_buf()
+							local clients = vim.lsp.get_clients({ bufnr = bufnr })
+							if #clients == 0 then
+								return " no LSP"
+							end
+							return " " .. clients[1].name
+						end,
+						cond = function()
+							return vim.fn.winwidth(0) > 60
+						end,
+					},
+					{
+						"encoding",
+						cond = function()
+							return vim.fn.winwidth(0) > 70
+						end,
+					},
+				},
+				lualine_y = {
+					{
+						"diff",
+						cond = function()
+							return vim.fn.winwidth(0) > 40
+						end,
+					},
+					{
+						"diagnostics",
+
+						cond = function()
+							return vim.fn.winwidth(0) > 40
+						end,
+					},
+				},
+				lualine_z = { "branch" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "progress" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
+		})
+	end,
+}
