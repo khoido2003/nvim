dec8ca2e7f9cbdcfb051bf7c328d7caf7fd41893
khoido2003 <khoizpro31@gmail.com>
2026-01-18 22:40:03 +0700

    feat: enable relative number

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -34,7 +34,7 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.lsp.set_log_level("off")
